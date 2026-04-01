f6dac62b30ceacb1aaef2be609f443afaa8115dc
Osdragbo21 <osvaldo.drago.21@gmail.com>
2026-04-01 00:27:27 -0600

    feat: agrega mutacion updateProducto y prepara endpoints para modulo de inventario

--- src/luna-vet/resolvers/producto/producto.resolver.ts
+++ src/luna-vet/resolvers/producto/producto.resolver.ts
@@ -2,10 +2,25 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { Producto } from '../../entities/productos/producto.entity';
 import { ProductoService } from '../../services/producto/producto.service';
 import { CreateProductoInput } from '../../dtos/producto/create-producto.input';
+import { UpdateProductoInput } from '../../dtos/producto/update-producto.input';
 
 @Resolver(() => Producto)
 export class ProductoResolver {
-    constructor(private readonly srv: ProductoService) {}
-    @Query(() => [Producto], { name: 'productos' }) findAll() { return this.srv.findAll(); }
-    @Mutation(() => Producto) createProducto(@Args('createInput') input: CreateProductoInput) { return this.srv.create(input); }
+  constructor(private readonly srv: ProductoService) {}
+  
+  @Query(() => [Producto], { name: 'productos' }) 
+  findAll() { 
+    return this.srv.findAll(); 
+  }
+  
+  @Mutation(() => Producto) 
+  createProducto(@Args('createInput') input: CreateProductoInput) { 
+    return this.srv.create(input); 
+  }
+
+  // NUEVA MUTACIÓN: Actualiza un producto existente
+  @Mutation(() => Producto)
+  updateProducto(@Args('updateProductoInput') updateProductoInput: UpdateProductoInput) {
+    return this.srv.update(updateProductoInput.id_producto, updateProductoInput);
+  }
 }
--- src/luna-vet/services/producto/producto.service.ts
+++ src/luna-vet/services/producto/producto.service.ts
@@ -3,10 +3,36 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Producto } from '../../entities/productos/producto.entity';
 import { CreateProductoInput } from '../../dtos/producto/create-producto.input';
+import { UpdateProductoInput } from '../../dtos/producto/update-producto.input';
 
 @Injectable()
 export class ProductoService {
-    constructor(@InjectRepository(Producto) private rep: Repository<Producto>) {}
-    findAll(): Promise<Producto[]> { return this.rep.find({ relations: ['proveedor'] }); }
-    create(input: CreateProductoInput): Promise<Producto> { return this.rep.save(this.rep.create(input)); }
+  constructor(@InjectRepository(Producto) private rep: Repository<Producto>) {}
+  
+  findAll(): Promise<Producto[]> { 
+    return this.rep.find({ relations: ['proveedor'] }); 
+  }
+  
+  create(input: CreateProductoInput): Promise<Producto> { 
+    return this.rep.save(this.rep.create(input)); 
+  }
+
+  // NUEVO MÉTODO: Actualizar producto (precios, stock, imágenes, etc.)
+  async update(id: number, updateInput: UpdateProductoInput): Promise<Producto> {
+    const producto = await this.rep.preload({
+      ...updateInput,
+      id_producto: id,
+    } as any);
+
+    if (!producto) {
+      throw new Error(`Producto con ID ${id} no encontrado`);
+    }
+
+    const productoActualizado = await this.rep.save(producto);
+
+    return this.rep.findOneOrFail({
+      where: { id_producto: productoActualizado.id_producto },
+      relations: ['proveedor']
+    });
+  }
 }
