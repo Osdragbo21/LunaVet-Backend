798889fbd1906db9cc56321cacd2b1ab168b1a38
Osdragbo21 <osvaldo.drago.21@gmail.com>
2026-04-04 02:35:31 -0600

    feat: agrega nombre_archivo a la entidad archivos_consulta para mejorar la UI de la galeria

--- src/luna-vet/dtos/archivoConsulta/create-archivo-consulta.input.ts
+++ src/luna-vet/dtos/archivoConsulta/create-archivo-consulta.input.ts
@@ -2,12 +2,16 @@ import { InputType, Field, Int } from '@nestjs/graphql';
 
 @InputType()
 export class CreateArchivoConsultaInput {
-    @Field(() => Int)
-    consulta_id: number;
+  @Field(() => Int)
+  consulta_id: number;
 
-    @Field()
-    url_archivo: string;
+  // NUEVO CAMPO OBLIGATORIO DESDE EL FRONTEND
+  @Field()
+  nombre_archivo: string;
 
-    @Field()
-    tipo_documento: string;
+  @Field()
+  url_archivo: string;
+
+  @Field()
+  tipo_documento: string;
 }
--- src/luna-vet/entities/archivos-consulta/archivo-consulta.entity.ts
+++ src/luna-vet/entities/archivos-consulta/archivo-consulta.entity.ts
@@ -5,28 +5,33 @@ import { Consulta } from '../consultas/consulta.entity';
 @ObjectType()
 @Entity('archivos_consulta')
 export class ArchivoConsulta {
-    @Field(() => Int)
-    @PrimaryGeneratedColumn()
-    id_archivo: number;
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id_archivo: number;
 
-    @Field(() => Int)
-    @Column()
-    consulta_id: number;
+  @Field(() => Int)
+  @Column()
+  consulta_id: number;
 
-    @Field(() => Consulta)
-    @ManyToOne(() => Consulta, (consulta) => consulta.archivos)
-    @JoinColumn({ name: 'consulta_id' })
-    consulta: Consulta;
+  @Field(() => Consulta)
+  @ManyToOne(() => Consulta, (consulta) => consulta.archivos)
+  @JoinColumn({ name: 'consulta_id' })
+  consulta: Consulta;
 
-    @Field()
-    @Column()
-    url_archivo: string;
+  // NUEVO CAMPO: Para mostrar en la Galería de la UI
+  @Field({ nullable: true })
+  @Column({ nullable: true })
+  nombre_archivo: string;
 
-    @Field()
-    @Column()
-    tipo_documento: string; // Ej: Radiografía, PDF Resultados
+  @Field()
+  @Column()
+  url_archivo: string;
 
-    @Field()
-    @CreateDateColumn()
-    fecha_subida: Date;
+  @Field()
+  @Column()
+  tipo_documento: string; // Ej: Radiografía, PDF Resultados
+
+  @Field()
+  @CreateDateColumn()
+  fecha_subida: Date;
 }
